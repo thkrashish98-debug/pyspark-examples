681e3f1c639b792b74126afcb06317ac468204b2
Ashish Thakur <thkrashish98@gmail.com>
2025-09-21 08:27:31 +0530

    reverting changes

--- test_folder/test_python.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql.functions import col, max
-
-df = spark.read.csv("/path_to_file")
-df.show(5,0)
-
-df1 = df.groupBy(col("country")).agg(max(col("salary")))
-
-df1.show(5,0)
-df1.write.csv("destination_path")
